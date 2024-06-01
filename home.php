56d7054ed0cf750544a39c85f08823112f7d180d
abdel <abdelnour.rekkab@gmail.com>
2024-06-01 16:15:50 +0200

    #

--- home.php
+++ home.php
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["executeButton"])) {
             <form method="post" action="game/start_game.php">
                 <h3 class="subtitle2"><i class="fa-solid fa-gamepad"></i>&emsp;Choisissez un mode de jeu&emsp;<i class="fa-solid fa-dice"></i></h3>
                 <button id="executeButton" class="btn btn-primary btn-lg" type="submit">Solo&emsp;<i class="fa-solid fa-user"></i></button>
-                <button id="executeButton" class="btn btn-primary btn-lg disabled" type="reset">Multijoueur&emsp;<i class="fa-solid fa-users"></i></button>
+                <button id="executeButton" class="btn btn-primary btn-lg" type="reset">Multijoueur&emsp;<i class="fa-solid fa-users"></i></button>
             </form>
         </main>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
